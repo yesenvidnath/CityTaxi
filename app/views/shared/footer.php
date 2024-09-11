b3ab8806a9539f3103e765593affc897908c8f24
Yesen Kandalama <70659937+yesenvidnath@users.noreply.github.com>
2024-09-11 20:06:28 +0530

    Delete app directory

--- app/views/shared/footer.php
+++
@@ -1,6 +0,0 @@
-    <!-- Bootstrap JS -->
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/js/bootstrap.bundle.min.js"></script>
-    <!-- Custom JS -->
-    <script src="/assets/js/script.js"></script>
-</body>
-</html>
